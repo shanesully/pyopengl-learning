76f3e86a539e253ed4aa9ef4d78219e40c974b98
sully <shanesully01@gmail.com>
2021-03-17 10:26:36 +0000

    Colour edges

--- pyopengl-cube.py
+++ pyopengl-cube.py
@@ -30,7 +30,21 @@ edges = (
     (5, 7)
 )
 
+surfaces = (
+    (0, 1, 2, 3),
+    (3, 2, 7, 6),
+    (6, 7, 5, 4),
+    (4, 5, 1, 0),
+    (1, 5, 7, 2),
+    (4, 0, 3, 6)
+)
+
 def Cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        glColor3fv((0, 1, 0))
+    glEnd()
+
     glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
